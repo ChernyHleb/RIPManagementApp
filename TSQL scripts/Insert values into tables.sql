c81d6b07ac7091914e1b7a890e49b23ffe764406
ChernyHleb <popovarlol@gmail.com>
2022-06-25 19:49:47 +0300

    Создал скрипт заполняющий таблицы значениями

---
+++ TSQL scripts/Insert values into tables.sql
@@ -0,0 +1,30 @@
+-- INSERT COMPANY VALUES --
+INSERT INTO [Company] VALUES ('Пятёрочка', 1)
+INSERT INTO [Company] VALUES ('Магнит', 1)
+INSERT INTO [Company] VALUES ('Лента', 1)
+INSERT INTO [Company] VALUES ('Перекрёсток', 0)
+INSERT INTO [Company] VALUES ('SPAR', 0)
+INSERT INTO [Company] VALUES ('Fix Price', 2)
+INSERT INTO [Company] VALUES ('Интим-бутик ЧЕРНАЯ КОШКА', 1)
+INSERT INTO [Company] VALUES ('К&Б', 1)
+
+-- INSERT USER VALUES --
+INSERT INTO [User] VALUES (1, 'Жожа Проблема', 'JojoTheProblem', 'ilovefrogs')
+INSERT INTO [User] VALUES (1, 'Никита Витальевич', 'chel_xorosh', 'password')
+INSERT INTO [User] VALUES (1, 'Jane Doe', 'jane_doe', 'woof')
+INSERT INTO [User] VALUES (1, 'Джеффри Рихтер', 'CLRviaCSharp', 'CLRviaCSharp')
+INSERT INTO [User] VALUES (1, 'Язагит Лера', 'lera2007', 'lera2007')
+
+INSERT INTO [User] VALUES (2, 'Василий Пупкин', 'vasiliy_pupkin', 'pupailupa')
+
+INSERT INTO [User] VALUES (3, 'Константин Павлович', 'zagadochniy', 'qwerty123')
+
+INSERT INTO [User] VALUES (7, 'Van Darkholme', 'dungeon_master', '300buks')
+INSERT INTO [User] VALUES (7, 'Billy Herrington', 'f******slave', 'weewee')
+INSERT INTO [User] VALUES (7, 'Олег Загадочный', 'MILFHUNTER3000', 'mhmhmhmh')
+INSERT INTO [User] VALUES (7, 'Дмитрий Гладченко', 'SOCKS_SNIFFER', '69696969')
+INSERT INTO [User] VALUES (7, 'Никита Степанов', 'latexDUDE69', 'latexandleather')
+
+INSERT INTO [User] VALUES (8, 'Дядя Ваня', 'dyadyavanya777', '777')
+
+
